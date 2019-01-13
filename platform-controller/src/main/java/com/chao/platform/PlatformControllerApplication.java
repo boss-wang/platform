f93360029c415d9e5b7a6d62104c99d14ed0b6d4
chao <261668497@qq.com>
2019-01-13 23:53:07 +0800

    first commit

---
+++ platform-controller/src/main/java/com/chao/platform/PlatformControllerApplication.java
@@ -0,0 +1,14 @@
+package com.chao.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformControllerApplication.class, args);
+    }
+
+}
+
---
+++ platform-controller/src/test/java/com/chao/platform/PlatformControllerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.chao.platform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlatformControllerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
